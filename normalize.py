eebe0c3c9d542261ef81894da9d0214d3ae18e28
Ferit <ferit.murad@student.uantwerpen.be>
2023-05-16 02:25:29 +0200

    normalized based of training data

--- normalize.py
+++ normalize.py
@@ -17,25 +17,23 @@ class Normalize:
         """
         path = os.getcwd() + "\\Training\\"
         mean_images = []
-        std_images = []
         for j in tqdm(self.json["Training"], desc="Calculating mean and std of Training set"):
             img_path = path + j + ".png"
             img = io.imread(img_path)
             img = img[:, :, :3]
-            mean_images.append([np.mean(img[:, :, 0]),np.mean(img[:, :, 1]),np.mean(img[:, :, 2])])
-            #std_images.append([np.std(img[:, :, 0]),np.std(img[:, :, 1]),np.std(img[:, :, 2])])
+            tensor_img = transforms.ToTensor()(img)
+            mean_images.append(torch.mean(tensor_img, dim=(1, 2)).tolist())
 
-        mean_images = np.array(mean_images)#, np.array(std_images)
-        mean, std = np.mean(mean_images, axis=0).tolist(), np.std(mean_images, axis=0).tolist()
+        # Convert the list to a NumPy array
+        mean_images_array = np.array(mean_images)
+
+        # Calculate the mean and standard deviation
+        mean = np.mean(mean_images_array, axis=0).tolist()
+        std = np.std(mean_images_array, axis=0).tolist()
+        
         print("Mean (R, G, B):", mean)
         print("Standard Deviation (R, G, B):", std)
 
-
-        
-        #print(mean)
-        #print(std)
-        
-        """
         for i in self.json:
             path = os.getcwd() + "\\" + i + "\\"
             for j in tqdm(self.json[i], desc="Normalizing " + str(i) + " set"):
@@ -43,9 +41,8 @@ class Normalize:
                 img = io.imread(img_path)
                 img = img[:, :, :3]
                 tensor_img = transforms.ToTensor()(img)
-                mean=[0.485, 0.456, 0.406]
-                std=[0.229, 0.224, 0.225]
+                #mean=[0.485, 0.456, 0.406]
+                #std=[0.229, 0.224, 0.225]
                 normalized_tensor_img = transforms.Normalize(mean=mean, std=std)(tensor_img)
                 pt_path = path + j + '.pt'
                 torch.save(normalized_tensor_img, pt_path)
-        """
